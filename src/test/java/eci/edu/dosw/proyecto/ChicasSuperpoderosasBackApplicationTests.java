5d4ea48a2c72b58ed04e81e00c211d551a9b56c4
Ana Fiquitiva <Ana.Fiquitiva-p@mail.esuelaing.edu.co>
2025-09-15 17:21:19 -0500

    Feat: Estructura Inicial de Back

---
+++ src/test/java/eci/edu/dosw/proyecto/ChicasSuperpoderosasBackApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.dosw.proyecto.CHICAS_SUPERPODEROSAS__BACK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChicasSuperpoderosasBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
