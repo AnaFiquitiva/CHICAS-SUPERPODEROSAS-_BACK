2dfc05b49870e0e79949638ebbeb8fa5f7fefdd7
Ana Fiquitiva <Ana.Fiquitiva-p@mail.esuelaing.edu.co>
2025-09-22 21:48:45 -0500

    Feat: Complementar del Scalfolding MVC

---
+++ src/main/java/eci/edu/dosw/proyecto/ChicasSuperpoderosasBackApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.dosw.proyecto.CHICAS_SUPERPODEROSAS__BACK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicasSuperpoderosasBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChicasSuperpoderosasBackApplication.class, args);
+	}
+
+}
