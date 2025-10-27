9f29fdb83a7647ce28c4d2e7137baf8d97676466
carlos astudillo <carlos.astudillo-c@mail.escuelaing.edu.co>
2025-10-26 20:50:40 -0500

    fix: Mongo db conectado

--- src/main/java/eci/edu/dosw/proyecto/ChicasSuperpoderosasBackApplication.java
+++ src/main/java/eci/edu/dosw/proyecto/ChicasSuperpoderosasBackApplication.java
@@ -2,12 +2,12 @@ package eci.edu.dosw.proyecto.CHICAS_SUPERPODEROSAS__BACK;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ChicasSuperpoderosasBackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChicasSuperpoderosasBackApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ChicasSuperpoderosasBackApplication.class, args);
+    }
 }
